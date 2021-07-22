bb8bfa976cfc40b06e2c6b5b328d129c74893c72
Pratty1205 <78639572+Pratty1205@users.noreply.github.com>
2021-07-22 22:45:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {}
+
+function setup() {
+    canvas = createCanvas(600, 450);
+    video = createCapture(VIDEO);
+    canvas.center();
+    video.size(600, 450);
+    video.hide();
+}
+
+function draw() {
+
+}
